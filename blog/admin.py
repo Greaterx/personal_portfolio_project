cb27a6f36524e7cc9ff51622ae58e903520d2475
Ivan Volontirec <ferdfred123@gmail.com>
2022-02-10 16:56:46 +0300

    test

---
+++ blog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from blog.models import my_blog
+
+admin.site.register(my_blog)
---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from blog.models import my_blog
+
+def all_blogs(request):
+    blogs = my_blog.objects.order_by('-date')
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(my_blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
